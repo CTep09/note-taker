a8f667e2dd9b62ddde4c2048cc6ca6523b134d32
Cassie T <cassandray9@gmail.com>
2023-04-27 16:21:55 -0600

    Code cleanup and added additional comments.

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,81 +1,78 @@
-const fs = require('fs')
-const path = require('path');
-const app = require('express').Router();
+// Importing modules
+const fs = require("fs");
+const path = require("path");
+const app = require("express").Router();
 
-const { v4: uuidv4 } = require('uuid');
-// const db = require('../db/db.json');
+// Generates unique id for each note
+const { v4: uuidv4 } = require("uuid");
 
+// GET /notes should read the db.json file and return all previously saved notes
 app.get("/notes", (req, res) => {
   const parsedNotes = getNotes();
   res.json(parsedNotes);
-  console.info(`${req.method} loaded saved notes`)
-  
-  function getNotes (){
-    const noteData = fs.readFileSync('./db/db.json', 'utf-8');
+  console.info(`${req.method} loaded saved notes`);
+
+  function getNotes() {
+    const noteData = fs.readFileSync("./db/db.json", "utf-8");
     const parsedNotes = JSON.parse(noteData);
-    
+
     return parsedNotes;
-  } 
-  // res.status(200).json(notes);
-  // res.sendFile(db);
-  
+  }
 });
 
+// POST /notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
 app.post("/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
-  
-  // Destructuring assignment for the items in req.body
-  const { title, text} = req.body;
-  // req.body.product;
+
+  // Destructuring for the items in req.body
+  const { title, text } = req.body;
   // If all the required properties are present
   if (title && text) {
-    // Variable for the object we will save
+    // Variable to save the new note object
     const newNote = {
       title,
       text,
       id: uuidv4(),
     };
     console.log(newNote);
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
         const parsedNotes = JSON.parse(data);
-        
-        parsedNotes.push(newNote)
-        
-        // Write the string to a file
+
+        parsedNotes.push(newNote);
+
+        // Write the updated notes back to the db.json file
         fs.writeFile(
-          './db/db.json',
+          "./db/db.json",
           JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
-          writeErr
-          ? console.error(writeErr)
-          : console.info('Successfully updated notes!')
-          );
-        }
-      });
-      
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-      
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting note');
-    }
-  });
-  
-  module.exports = app 
-  
-  // app.delete("/notes/:id", (req, res) => {
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated notes!")
+        );
+      }
+    });
     
-    // })
+    // Send JSON response with new note and success status
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
     
-// The following API routes should be created:
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    // If there is an error, console log the error
+    res.status(500).json("Error in posting note");
+  }
+});
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
+  module.exports = app;
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// app.delete("/notes/:id", (req, res) => {
+
+// })
--- router/htmlRouter.js
+++ router/htmlRouter.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const app = require('express').Router();
+// Importing modules
+const path = require("path");
+const app = require("express").Router();
 
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '../public/notes.html'))
-)
 
-app.get('/assets/css/styles.css', (req, res) => 
-res.sendFile(path.join(__dirname, '../public/assets/css/styles.css'))
-)
-
-app.get('/assets/js/index.js', (req, res) => 
-res.sendFile(path.join(__dirname, '../public/assets/js/index.js'))
-)
-
-// GET * should return the index.html file.
-app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '../public/index.html'))
+// Get route for /notes server will respond with sending notes.html
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
+);
+// Get route for style.css so style will render correctly on the page
+app.get("/assets/css/styles.css", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"))
+);
+// Get route for index.js for proper functioning of website
+app.get("/assets/js/index.js", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/assets/js/index.js"))
 );
 
-module.exports = (app) 
-// The following HTML routes should be created:
+// GET * should return the index.html file.
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
-// GET /notes should return the notes.html file.
-
+// Exporting router
+module.exports = app;
 
